6f39e7d8af70eb0112004f01e3f3d368ce5e3232
rohitlavate97 <rohitlavate97@gmail.com>
2024-12-05 09:27:57 +0530

    added programmatic approach, added @PostConstruct and @PreDestroy annotations

--- src/main/java/in/alchemist/beans/UserDAO.java
+++ src/main/java/in/alchemist/beans/UserDAO.java
@@ -2,22 +2,29 @@ package in.alchemist.beans;
 
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
-//for programmatic approach we have to implement InitializingBean and DisposableBean Interfaces
-public class UserDAO implements InitializingBean, DisposableBean {
 
-    //    public void init(){
-//        System.out.println("Getting db  connection");
-//    }
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        System.out.println("This is init method : Getting db  connection");
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+//for programmatic approach we have to implement InitializingBean and DisposableBean Interfaces
+@Component     //required as using annotation
+public class UserDAO  {
+    @PostConstruct
+    public void init(){
+        System.out.println("Getting db  connection");
     }
+//    @Override
+//    public void afterPropertiesSet() throws Exception {
+//        System.out.println("This is init method : Getting db  connection");
+// code commented for annotation approach
+//    }
 
     public void getData() {
         System.out.println("Getting data from user");
     }
-
+    @PreDestroy
     public void destroy() {
         System.out.println("closing db  connection");
     }
